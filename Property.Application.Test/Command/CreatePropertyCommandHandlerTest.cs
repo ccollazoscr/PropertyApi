9bc78422cbcafc86fb72167cfd406a518a144c53
ccollazoscr <coll33772@siigo.com>
2021-11-28 16:21:25 -0500

    Remove references

--- Property.Application.Test/Command/CreatePropertyCommandHandlerTest.cs
+++ Property.Application.Test/Command/CreatePropertyCommandHandlerTest.cs
@@ -6,10 +6,6 @@ using Property.Application.Port;
 using Property.Common.Exception;
 using Property.Model.Dto;
 using Property.Model.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Property.Application.Test.Command
